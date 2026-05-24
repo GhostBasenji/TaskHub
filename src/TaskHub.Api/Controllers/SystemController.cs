b9c1ee48d2da6aa00e75bcc43ea1a6720e1a2ee7
Andrey P <petr.and.vic@gmail.com>
2026-05-25 03:12:14 +0400

    part-02 prepare project structure

---
+++ src/TaskHub.Api/Controllers/SystemController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskHub.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SystemController : ControllerBase
+{
+    [HttpGet("ping")]
+    public IActionResult Ping()
+    {
+        return Ok("TaskHub API is running");
+    }
+}
